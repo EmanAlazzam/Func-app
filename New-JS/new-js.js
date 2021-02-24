17c0bf56998416159b6924d50af595405697d97c
Eman Alazzam <79112414+EmanAlazzam@users.noreply.github.com>
2021-02-24 23:12:14 +0200

    Update new-js.js

--- New-JS/new-js.js
+++ New-JS/new-js.js
@@ -7,15 +7,17 @@ for (var i=0; i<1;i++){
 if (gameM==7) {
     alert("YAAAAAAAAAY YOU ARE RIGHT IT IS 7!")
 
-    for(var j=0;j<7;j++){
+    } else {
+    alert("YOU ENTERED A WRONG NUMBER!!");
+i-=1;
+}
+
+}
+var numberOfPic=prompt("How many picture of the World Wonders?")
+for(var j=1;j<=numberOfPic;j++){
         document.write('<img src="https://i.pinimg.com/originals/9d/b0/e5/9db0e56d405b4bcf39c0f48a8580dd70.jpg"/> ')
 
 
     
-
-}} else {
-    alert("YOU ENTERED A WRONG NUMBER!!");
-i-=1;
-}
 
 }
