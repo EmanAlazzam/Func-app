dc0eb6e3c3f7c58b6709a8d0e247d1e4d17178c1
EmanAlazzam <emanalazzam577@gmail.com>
2021-02-24 21:27:42 +0200

    created in VS

---
+++ New-JS/new-js.js
@@ -0,0 +1,21 @@
+confirm("Smile Please!!")
+confirm("EDUCATE YOURSELF!!")
+
+
+for (var i=0; i<1;i++){
+    var gameM= prompt("HOW MANY WORLD WONDERS ARE THERE?!");
+if (gameM==7) {
+    alert("YAAAAAAAAAY YOU ARE RIGHT IT IS 7!")
+
+    for(var j=0;j<7;j++){
+        document.write('<img src="https://i.pinimg.com/originals/9d/b0/e5/9db0e56d405b4bcf39c0f48a8580dd70.jpg"/> ')
+
+
+    
+
+}} else {
+    alert("YOU ENTERED A WRONG NUMBER!!");
+i-=1;
+}
+
+}
